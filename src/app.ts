0d502426dd33f060b2973846fc77625fca34802a
Petr Lipatov <lipatov3@gmail.com>
2025-01-19 14:41:16 +0300

    feat: static files middleware added

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 const port = 3300;
 
 app.use(cors());
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
 app.use("/", dataRoutes);
 app.use("/", uploadRoutes);
