2859365e2ea7654c0c1fa966fedc5227b45fbb23
Petr Lipatov <lipatov3@gmail.com>
2025-01-19 15:15:59 +0300

    fix: static files path updated

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = 3300;
 
 app.use(cors());
-app.use("/images", express.static(path.join(__dirname, "public", "images")));
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
 app.use(express.json());
 app.use("/", dataRoutes);
 app.use("/", uploadRoutes);
