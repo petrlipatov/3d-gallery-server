f310edc1de9930a42cc523c9e3d3845514c14b4f
Petr Lipatov <lipatov3@gmail.com>
2025-01-19 18:10:07 +0300

    chore: CORS config updated

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,13 @@ const dataRoutes = require("./routes/dataRoutes");
 const app = express();
 const port = 3300;
 
-var corsOptions = {
-  origin: "https://stepanplusdrawingultra.site",
-  optionsSuccessStatus: 200,
-};
+// var corsOptions = {
+//   origin: "https://stepanplusdrawingultra.site",
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "../public/images")));
 app.use(express.json());
 app.use("/", dataRoutes);
