2ab431eef6784bb88f07af6dd32d1fae18afabe1
Petr Lipatov <lipatov3@gmail.com>
2025-01-27 20:07:15 +0300

    chore: CORS conf updated

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,15 @@ const app = express();
 const port = 3300;
 
 var corsOptions = {
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   origin: "https://stepanplusdrawingultra.site",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+
   optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.use("/", imagesRoutes);
